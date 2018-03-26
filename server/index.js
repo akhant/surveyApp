51b8bb59f7380e0a64310fc4140c07eae98a4abf
akhant <a.khantelev@mail.ru>
2018-03-26 20:05:42 +0300

    new2

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req,res)=> {
-    res.send({Hello: "world"})
+    res.send("world")
 })
 
 const PORT = process.env.PORT || 5000
@@ -13,5 +13,5 @@ app.listen(PORT, ()=> {
 //for heroku deploy:
 //1.Dinamic Port
 //2.Node and Npm version in package.json
-//3.Start script in pachage.json
+//3.Start script in package.json
 //4.Add .gitignore file
