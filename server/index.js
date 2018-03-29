3f79f6762604304cfc02ac7af72031e408072dd6
akhant <a.khantelev@mail.ru>
2018-03-29 12:07:03 +0300

    path

--- server/index.js
+++ server/index.js
@@ -26,13 +26,14 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
-console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV === "undefined") {
+
+if (process.env.NODE_ENV === "production") {
+  console.log("dirname", __dirname)
   app.use(express.static("client/build"));
 
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
