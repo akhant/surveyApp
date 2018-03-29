932e0068d679b393901267e9a959c17b5f89bebd
akhant <a.khantelev@mail.ru>
2018-03-29 12:41:04 +0300

    path2

--- server/index.js
+++ server/index.js
@@ -28,12 +28,12 @@ require("./routes/billingRoutes")(app);
 
 
 if (process.env.NODE_ENV === "production") {
-  console.log("dirname", __dirname)
+  
   app.use(express.static("client/build"));
 
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
 }
 
